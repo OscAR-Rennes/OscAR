1c28c03d896411df22a3e1510b3c3026fd071ee4
Celoma <salouet.clement049@gmail.com>
2025-11-20 16:57:48 +0100

    r

--- frontend-desktop/src/App.js
+++ frontend-desktop/src/App.js
@@ -3,7 +3,7 @@ function App() {
     <div>
       <h1>Test de clément pdour le CICD 2222</h1>
       <h2>Une autre ligne</h2>
-      <h3>Une ytoidse</h3>
+      <h3>Une ytoidseddd</h3>
     </div>
   );
 }
