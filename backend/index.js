af216e84d59d3c1fd5d5b58f3eac9f1840d78efd
Celoma <salouet.clement049@gmail.com>
2025-11-21 16:38:28 +0100

    login

--- backend/index.js
+++ backend/index.js
@@ -9,7 +9,7 @@ app.use(cors({
 }));
 
 app.get('/api/hello', (req, res) => {
-  res.json({ message: 'Heddlldddod ffdfdfdrosfffdme gfdfffbackvcbvcbddend!' });
+  res.json({ message: 'Heddlldddfdod ffdfdfdrosfffdme gfdfffbackvcbvcbddend!' });
 });
 
 // Utiliser le port fourni par Railway
