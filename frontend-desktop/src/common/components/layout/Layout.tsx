074ba139d1c56e3abf7d7f585c1fc9404f1eb6c9
Yanis <gachot.yanis@gmail.com>
2026-02-18 20:40:38 +0100

    YGA - Header + Sidebar

--- frontend-desktop/src/common/components/layout/Layout.tsx
+++ frontend-desktop/src/common/components/layout/Layout.tsx
@@ -1,15 +1,19 @@
 import { Outlet } from "react-router-dom";
 import Header from "../header/Header";
-
+import Sidebar from "../Sidebar/Sidebar";
+import "./Layout.style.css";
 
 export default function Layout() {
   return (
-    <>
+    <div className="layout-root">
       <Header />
-      <main>
-        <Outlet />
-      </main>
+      <div className="layout-content">
+        <Sidebar />
+        <main className="layout-main">
+          <Outlet />
+        </main>
+      </div>
       {/* <Footer /> */}
-    </>
+    </div>
   );
 }
