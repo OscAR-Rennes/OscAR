c26089612adc78bab5b4c8020f67e521e773ae9a
Yanis <gachot.yanis@gmail.com>
2026-03-23 12:05:51 +0100

    ajout modal logout et suppression parametres

--- frontend-desktop/src/common/components/Sidebar/Sidebar.tsx
+++ frontend-desktop/src/common/components/Sidebar/Sidebar.tsx
@@ -2,6 +2,8 @@ import './Sidebar.style.css';
 import { useAuthStore } from "../../store/authStore";
 import { useLocation, useNavigate } from "react-router-dom";
 import { logoutUser } from "../../../api/services/auth.api";
+import { useState } from "react";
+import ConfirmModal from "../confirmmodal/ConfirmModal";
 
 const menuItemsBase = [
   { icon: <img src={require("../../assets/icon/dashboard.svg").default} alt="Dashboard"/>, label: "Tableau de bord", path: "/home/dashboard" },
@@ -15,10 +17,13 @@ const settingsIcon = <img src={require("../../assets/icon/settings.svg").default
 const logoutIcon = <img src={require("../../assets/icon/logout.svg").default} alt="logout" />;
 
 const Sidebar = () => {
+
   const userRights = useAuthStore((state) => state.user?.rights ?? []);
   const clearUser = useAuthStore((state) => state.clearUser);
   const navigate = useNavigate();
   const location = useLocation();
+
+  const [isLogoutModalOpen, setIsLogoutModalOpen] = useState(false);
 
   const handleLogout = async () => {
     await logoutUser();
@@ -72,20 +77,25 @@ const Sidebar = () => {
           )}
         </ul>
         <div className="sidebar-settings-container">
-          <li className="sidebar-settings-item">
-            <span className="sidebar-icon">{settingsIcon}</span>
-            <span className="sidebar-label">Paramètres</span>
-          </li>
-          <li className="sidebar-settings-item" onClick={handleLogout}>
+          <li className="sidebar-settings-item" onClick={() => setIsLogoutModalOpen(true)}>
             <span className="sidebar-icon">{logoutIcon}</span>
             <span className="sidebar-label">Se déconnecter</span>
           </li>
         </div>
       </nav>
       <div className="sidebar-navigation">
-        <button onClick={() => navigate('/')}>
+        <button onClick={() => navigate('/')}> 
         </button>
       </div>
+      <ConfirmModal
+        isOpen={isLogoutModalOpen}
+        message="Êtes-vous sûr de vouloir vous déconnecter ?"
+        onCancel={() => setIsLogoutModalOpen(false)}
+        onConfirm={() => {
+          setIsLogoutModalOpen(false);
+          handleLogout();
+        }}
+      />
     </div>
   );
 };
